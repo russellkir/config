fff8b17679f74afb8d4f7834cdc2ba0cb69f8186
Russell Kirmayer <russellkir@gmail.com>
2018-03-25 18:26:47 -0400

    Initial commit

---
+++ dotfiles/.vimrc
@@ -0,0 +1,8 @@
+" Russell Kirmayer custom vimrc file
+
+" Show line numbers
+set number
+
+" Proper spacing and formatting
+set tabstop=4
+set expandtab
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install script for dot files and whatnot
+
+# Custom vimrc file
+vimrc_file="/home/russellkir/.vimrc"
+
+if [ -f $vimrc_file ]
+then
+    echo "Removing old .vimrc file"
+	rm $vimrc_file
+fi
+
+echo "Linking new .vimrc file"
+ln -sv "$PWD/dotfiles/.vimrc" ~ > /dev/null
+
+# Custom gitconfig file
+gitconfig_file="/home/russellkir/.gitconfig"
+
+if [ -f $gitconfig_file ]
+then
+    echo "Removing old .gitconfig file"
+    rm $gitconfig_file
+fi
+
+echo "Linking new .gitconfig file"
+ln -sv "$PWD/dotfiles/.gitconfig" ~ > /dev/null
